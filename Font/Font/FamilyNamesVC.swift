c96803a2efbf668872f759fea1efec21a8fe28f2
HanYanFeng <934672137@qq.com>
2018-03-15 09:30:30 +0800

    e
    e

--- Font/Font/FamilyNamesVC.swift
+++ Font/Font/FamilyNamesVC.swift
@@ -1,17 +1,15 @@
 //
-//  ViewController.swift
+//  FamilyNamesVC.swift
 //  Font
 //
-//  Created by 韩艳锋 on 2017/11/20.
-//  Copyright © 2017年 韩艳锋. All rights reserved.
+//  Created by 韩艳锋 on 2018/3/15.
+//  Copyright © 2018年 韩艳锋. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource{
-
+class FamilyNamesVC: UIViewController,UITableViewDelegate, UITableViewDataSource {
     var tableView : UITableView = UITableView()
-    var dataSource = ["FamilyNames"]
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.frame = CGRect(x: 0, y: 64, width: self.view.frame.width, height: self.view.frame.height - 64)
@@ -19,37 +17,28 @@ class ViewController: UIViewController ,UITableViewDelegate,UITableViewDataSourc
         tableView.dataSource = self
         self.view.addSubview(tableView)
         self.view.backgroundColor = UIColor.white
-        self.title = "字体"
+        self.title = "familyNames"
     }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataSource.count
+        return UIFont.familyNames.count
     }
-    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
         if cell == nil {
             cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
         }
-        cell?.textLabel?.text = dataSource[indexPath.row]
+        cell?.textLabel?.text = UIFont.familyNames[indexPath.row]
+        cell?.textLabel?.font = UIFont(name:(cell?.textLabel?.text)! , size: 17)
         return cell!
     }
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         tableView.deselectRow(at: indexPath, animated: true)
-        var vc : UIViewController?
-        switch dataSource[indexPath.row] {
-        case "FamilyNames":
-             vc = FamilyNamesVC()
-        default:
-            break
-        }
-        self.navigationController?.pushViewController(vc!, animated: true)
+        let fontDetailVC = FontDetailVC()
+        fontDetailVC.familyName = UIFont.familyNames[indexPath.row]
+        self.navigationController?.pushViewController(fontDetailVC, animated: true)
     }
 }
-
